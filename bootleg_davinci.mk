cdec92b5e88d39162804e1c82b0604023f658187
joshelior <nwokochajoshua@gmail.com>
2020-10-29 08:18:38 +0000

    Revert "it's mallu time :)"
    This reverts commit 5d9dcd69624fbe1169535bcd15153e3235afdc2a.

--- bootleg_davinci.mk
+++ bootleg_davinci.mk
@@ -22,12 +22,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/davinci/device.mk)
 
 # Inherit some common BootLeggers stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/bootleggers/config/common.mk)
 TARGET_USES_BLUR := true
+
+# Maintainer Prop
+PRODUCT_BUILD_PROP_OVERRIDES += \
+DEVICE_MAINTAINERS="MR.ROBOT.MK"
 
 TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := lineage_davinci
+PRODUCT_NAME := bootleg_davinci
 PRODUCT_DEVICE := davinci
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 9T
